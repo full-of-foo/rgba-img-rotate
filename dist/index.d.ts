e7660bc47024392369a74264762701e4f1ba2a75
Tony Troy <lightweightdevelopment@gmail.com>
2020-04-29 13:27:30 +0100

    Refactor prep for degree rotation support

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,7 +4,15 @@ declare class PixelArrayRotator {
     heigth: number;
     constructor(data: Array<number>, w: number, h: number);
     getPixelStartIndexForCoord(x: number, y: number): number;
-    rotate(): Array<number>;
+    private rotate90;
+    private rotate180;
+    private rotate270;
+    /**
+     * Rotates the given one-dimensional array by the given right angle in degrees.
+     * Degrees must be a multiple of 90. Returns the original input if told to
+     * rotate by 0 or 360 degrees.
+     */
+    rotate(degrees?: number): Array<number>;
 }
 declare class ImageDataRotator {
     static rotate(image: ImageData, angle: number): ImageData;
--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,7 @@ class PixelArrayRotator {
     return (x + y * this.width) * 4;
   }
 
-  // Assumes just 90 from (0,0)
-  rotate(): Array<number> {
+  private rotate90(): Array<number> {
     let index;
     const rotatedArray = [];
     console.log(this.pixelArray, this.width, this.heigth);
@@ -31,6 +30,39 @@ class PixelArrayRotator {
 
     console.log(rotatedArray);
     return rotatedArray;
+  }
+
+  private rotate180(): Array<number> {
+    // TODO: implement
+    return this.pixelArray;
+  }
+
+  private rotate270(): Array<number> {
+    // TODO: implement
+    return this.pixelArray;
+  }
+
+  /**
+   * Rotates the given one-dimensional array by the given right angle in degrees.
+   * Degrees must be a multiple of 90. Returns the original input if told to
+   * rotate by 0 or 360 degrees.
+   */
+  rotate(degrees = 90): Array<number> {
+    if (degrees % 90 !== 0) {
+      throw new Error('Invalid input; degrees must be a multiple of 90');
+    }
+    const angle = ((degrees % 360) + 360) % 360;
+
+    if (angle === 90) {
+      return this.rotate90();
+    }
+    if (angle === 180) {
+      return this.rotate180();
+    }
+    if (angle === 270) {
+      return this.rotate270();
+    }
+    return this.pixelArray;
   }
 }
 
